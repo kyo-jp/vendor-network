3bf7236a2076393615632955f41be8d413ddb2d7
uekyo <kyo@viv.co.jp>
2023-11-23 23:10:41 -0800

    change private2

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -34,7 +34,7 @@ resource "aws_subnet" "private_2" {
   availability_zone       = var.availability_zones[1]
   map_public_ip_on_launch = true
   tags = {
-    Name = "${var.app_name}-private-1"
+    Name = "${var.app_name}-private-2"
   }
 }
 
