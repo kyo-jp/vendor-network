cb5aeef75ae40cc13da6b0456d1c022b15046028
uekyo <kyo@viv.co.jp>
2023-11-23 17:51:24 -0800

    add rt debug 2

--- terraform/route_table.tf
+++ terraform/route_table.tf
@@ -32,7 +32,7 @@ resource "aws_route" "public_2" {
 }
 
 resource "aws_route_table_association" "public_2" {
-  subnet_id      = aws_subnet.public_1.id
+  subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public_2.id
 }
 
