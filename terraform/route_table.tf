9125caa974f8cb3439600ba4380d915038ab7506
uekyo <kyo@viv.co.jp>
2023-11-23 17:53:32 -0800

    add rt debug 3

--- terraform/route_table.tf
+++ terraform/route_table.tf
@@ -71,6 +71,6 @@ resource "aws_route" "private_2" {
 }
 
 resource "aws_route_table_association" "private_2" {
-  subnet_id      = aws_subnet.private_1.id
+  subnet_id      = aws_subnet.private_2.id
   route_table_id = aws_route_table.private_2.id
 }
