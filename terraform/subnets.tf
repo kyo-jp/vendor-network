b424c3587b805c3b31571a6b49afb7bc34ddb1a8
uekyo <kyo@viv.co.jp>
2023-11-23 17:21:18 -0800

    debug subnets

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "public_2" {
   }
 }
 
-resource "aws_subnet" "public_1" {
+resource "aws_subnet" "private_1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private_subnets[0]
   availability_zone       = var.availability_zones[0]
@@ -28,7 +28,7 @@ resource "aws_subnet" "public_1" {
   }
 }
 
-resource "aws_subnet" "public_2" {
+resource "aws_subnet" "private_2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private_subnets[1]
   availability_zone       = var.availability_zones[1]
